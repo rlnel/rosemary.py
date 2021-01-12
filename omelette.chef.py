8c29b89089b8eba521ae3641f7d18b0271979f31
rlnel <r.l.nel@students.uu.nl>
2021-01-12 17:08:11 +0100

    2 omlettes

--- omelette.chef.py
+++ omelette.chef.py
@@ -1,18 +1,22 @@
 from kitchen import Rosemary
-from kitchen.utensils import Pan, Plate
+from kitchen.utensils import Pan, Plate, Bowl
 from kitchen.ingredients import Butter, Egg, Salt
 
-egg = Egg.take()
-egg.crack()
+bowl = Bowl.use(name='egg')
+for egg in Egg.take(2):
+    egg.crack()
+    bowl.add(egg)
+bowl.mix()
 
 pan = Pan.use(name='omelette')
-pan.add(Butter.take('slice'))
-pan.add(egg)
-pan.add(Salt.take('dash'))
-pan.cook(minutes=2)
-
 plate = Plate.use()
-omelette = pan.take()
-plate.add(omelette)
+for i in range(2): 
+    pan.add(Butter.take('slice'))
+    pan.add(bowl.take('1/2'))
+    pan.add(Salt.take('dash'))
+    pan.cook(minutes=4)
+
+    omelette = pan.take()
+    plate.add(omelette)
 
 Rosemary.serve(plate)
